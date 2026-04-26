ce17490b0fe615ced2031edfd86408068e0ff8ee
chlo6 <chloe.he.2019@gmail.com>
2026-04-26 07:07:37 +0000

    datapath

--- src/utils.py
+++ src/utils.py
@@ -17,15 +17,24 @@ def get_dataset(args):
 
     if args.dataset == 'cifar':
         data_dir = '/home/coder/Federated-Learning-PyTorch/data/cifar/'
-        apply_transform = transforms.Compose(
-            [transforms.ToTensor(),
-             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+        apply_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                (0.2023, 0.1994, 0.2010))])
+
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                (0.2023, 0.1994, 0.2010))])
 
         train_dataset = datasets.CIFAR10(data_dir, train=True, download=True,
-                                       transform=apply_transform)
+                                        transform=apply_transform)
 
         test_dataset = datasets.CIFAR10(data_dir, train=False, download=True,
-                                      transform=apply_transform)
+                                        transform=test_transform)
 
         # sample training data amongst users
         if args.iid:
